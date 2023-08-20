a9b19e7b8f22dd2bbc14a40bbecf6b567c68e365
erknyldrm <erkan-84@hotmail.com>
2023-08-20 11:27:39 +0300

    Add project files.

---
+++ Controllers/OpenAICompletionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.Interfaces;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels.ResponseModels;
+
+namespace OpenAI.API.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAICompletionController : ControllerBase
+    {
+        private readonly IOpenAIService _openAIService;
+
+        public OpenAICompletionController(IOpenAIService openAIService)
+        {
+            _openAIService = openAIService;
+        }
+
+        [HttpGet]
+        [Route("CreateCompletion")]
+        public async Task<string> CreateCompletion(string question)
+        {
+            CompletionCreateResponse response = await _openAIService.Completions.CreateCompletion(new CompletionCreateRequest
+            {
+                Prompt = question,
+                MaxTokens = 500
+            }, Models.TextDavinciV3);
+
+            return response.Choices[0].Text;
+        }
+    }
+}
---
+++ Controllers/OpenAIImageController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OpenAI.Interfaces;
+using OpenAI.ObjectModels;
+using OpenAI.ObjectModels.ResponseModels.ImageResponseModel;
+
+namespace OpenAI.API.Example.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAIImageController : ControllerBase
+    {
+        private readonly IOpenAIService _openAIService;
+
+        public OpenAIImageController(IOpenAIService openAIService)
+        {
+            _openAIService = openAIService;
+        }
+
+        [HttpGet]
+        [Route("CreateImage")]
+        public async Task<IEnumerable<string>> CreateImage(string question)
+        {
+            ImageCreateResponse response = await _openAIService.Image.CreateImage(new ObjectModels.RequestModels.ImageCreateRequest
+            {
+                Prompt = question,
+                N = 2,
+                Size = StaticValues.ImageStatics.Size.Size256,
+                ResponseFormat = StaticValues.ImageStatics.ResponseFormat.Url,
+                User = "Test"
+            });
+
+            return response.Results.Select(p => String.Join("\n", p.Url));
+        }
+
+    }
+}
